e0c0d550a8288ad0f553163ccff75b15c42cd87b
feiy <648039341@qq.com>
2022-02-11 11:01:46 +0800

    yan

--- src/views/customer/CustomerList.vue
+++ src/views/customer/CustomerList.vue
@@ -41,7 +41,7 @@
 								<el-button size="small" type="primary">导入excel数据</el-button>
 							</el-upload>
 							<div class="download-icon">
-								<a href="/static/file/客户信息模板.xlsx" class="link"><img src="@/assets/img/icon/download@icon.png"> 下载模板</a>
+								<a href="http://api.szfxws.com/static/file/客户信息模板.xlsx" class="link"><img src="@/assets/img/icon/download@icon.png"> 下载模板</a>
 							</div>
 						</div>
 						

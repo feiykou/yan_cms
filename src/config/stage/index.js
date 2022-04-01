5369e90b7ca6cfa29dc13536422a306ab853f409
feiy <648039341@qq.com>
2022-04-01 15:59:31 +0800

    yan

--- src/config/stage/index.js
+++ src/config/stage/index.js
@@ -13,7 +13,6 @@ import CustomerConfig from './customer'
 import CustomerProjectConfig from './customerProject'
 import StaticsConfig from './statics'
 import Config from './config'
-import pluginsConfig from './plugins'
 import Utils from '@/lin/utils/util'
 
 // eslint-disable-next-line import/no-mutable-exports
@@ -56,7 +55,7 @@ let homeRouter = [{
     Config
 ]
 
-const plugins = [...pluginsConfig]
+// const plugins = [...pluginsConfig]
 
 // 筛除已经被添加的插件
 function filterPlugin(data) {
@@ -79,9 +78,10 @@ function filterPlugin(data) {
     }
 }
 
-filterPlugin(homeRouter)
+// filterPlugin(homeRouter)
 
-homeRouter = homeRouter.concat(plugins)
+// homeRouter = homeRouter.concat(plugins)
+
 
 // 处理顺序
 homeRouter = Utils.sortByOrder(homeRouter)
